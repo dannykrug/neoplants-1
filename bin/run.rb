d8df8f8d5017cf611b76fcf567881782ee2ec413
joyhuangg <joyhuangg@gmail.com>
2018-09-13 12:05:18 -0400

    Incorporating science trivia questions triggered upon actions

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,5 @@
 require_relative "../lib/api_communicator.rb"
 
 get_random_question_from_api
+binding.pry
+0
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,21 +3,13 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+#can call on return value with ["question"], ["correct_answer"], ["incorrect_answers"]#array
 def get_random_question_from_api
-  # result = []
-  binding.pry
   response_string = RestClient.get('https://opentdb.com/api.php?amount=50&category=17&type=multiple')
   response_hash = JSON.parse(response_string)
-
-  while response_hash["next"] != nil
-    response_hash["results"].each do |character_info|
-      # binding.pry
-      if character_info["name"].downcase == character
-        return character_info
-      end
-    end
-    response_string = RestClient.get(response_hash["next"])
-    response_hash = JSON.parse(response_string)
-  end
-  return nil
+  #get random question
+  size = response_hash["results"].count
+  rand_i = rand(size)
+  return response_hash["results"][rand_i]
+  #return random question with keys "question", "correct_answer", "incorrect_answers"
 end
